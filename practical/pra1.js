3fd25ac1d9228d917d37a4df9beee76276458223
darshgarala <110333279+darshgarala@users.noreply.github.com>
2023-07-26 21:44:15 +0530

    Create pra1.js

---
+++ practical/pra1.js
@@ -0,0 +1,15 @@
+const name = "darsh";
+const age = 22;
+const height = 5.5;
+const t_f = true;
+
+console.log(name);
+console.log(age);
+console.log(height);
+console.log(t_f);
+
+function sum_of_two(s1, s2) {
+  return s1 + s2;
+}
+
+console.log(sum_of_two(10.23, 20.46));
